fc63d60c01d7c469c96cd2dbf900f015f803ed98
Virginia Vazquez Valderrama <vicky@MacBook-Air-de-Virginia.local>
2019-02-08 21:06:40 -0600

    calculadora con suma, resta, multiplicacion y division

---
+++ index.js
@@ -0,0 +1,26 @@
+// var nombre = "Virginia";
+// let edad = 26;
+// const cabello = "oscuro"
+//
+// // Concepto de funcion
+
+// console.log ('hola')
+// function saludar (){
+//   console.log ('hola a todos')
+// }
+//
+// saludar ();
+
+// let nombre = "Virginia";
+//
+// function saludar (parametroDeFuncion){
+//   console.log ('hola '+ parametroDeFuncion)
+// }
+//
+// saludar (nombre);
+
+const math = require ('./math');
+console.log(math.suma(10,20));
+console.log(math.resta(20,10));
+console.log(math.multiplicacion(10,5));
+console.log(math.division(20,2));
---
+++ math.js
@@ -0,0 +1,22 @@
+// function suma (a,b){
+//   return a+b;
+// }
+
+// Para exportarlo como un modulo
+module.exports = {
+  // funcion anonima, no tiene nombre
+  suma: function (a,b){
+    return a+b;
+  },
+  resta: function (a,b){
+    return a - b;
+  },
+  multiplicacion: function (a,b){
+    return a * b;
+  },
+  division: function (a,b){
+    return a / b;
+  }
+}
+
+// como acceder a la propiedad de nuestro objeto
